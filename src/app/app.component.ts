4e4551b4bbb3b95c0296e8fd91d032982a2b1e81
Inti <intimartinezfloresicube@gmail.com>
2024-02-13 19:38:49 -0600

    Creando form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'envioCorreos';
+
+  form:FormGroup;
+
+  constructor(private httpClient:HttpClient){
+    this.form = new FormGroup({
+      correo: new FormControl('', [Validators.required, Validators.email]),
+      asunto: new FormControl('', Validators.required),
+      mensaje: new FormControl('', Validators.required)
+    })
+  }
+
+  enviarCorreo(){
+    let params = {
+      email:this.form.value.correo,
+      asunto:this.form.value.asunto,
+      mensaje:this.form.value.mensaje
+    }
+
+    this.httpClient.post('http://localhost:3000/envio',params).subscribe(res => {
+      console.log(res);
+      alert("Correo enviado");
+    })
+  }
+
 }
+
+
